ec91f53c67b5b4ab7a484c4951b9c4148281c10f
aqiu-gitee <1223357035@qq.com>
2023-02-20 12:49:21 +0800

    根据id修改分类信息

--- src/main/java/com/aqiu/reggie/controller/CategoryController.java
+++ src/main/java/com/aqiu/reggie/controller/CategoryController.java
@@ -79,4 +79,17 @@ public class CategoryController {
 
     }
 
+    /**
+     * 根据id修改分类信息
+     * @param category
+     * @return
+     */
+    @PutMapping
+    public R<String> update(@RequestBody Category category){
+        log.info("修改分类信息{}",category);
+        categoryService.updateById(category);
+
+        return R.success("修改成功。");
+    }
+
 }
